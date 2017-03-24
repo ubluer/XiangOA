27a0dcf1919a6e8d12ce2b10c6462b10346a39c1
xiang.yu <yuxiang0732@qq.com>
2017-03-24 15:10:34 +0800

    表单前后传值

---
+++ src/main/java/com/xiang/modules/crm/api/CrmCustomerApi.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright &copy; 2012-2016 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.xiang.modules.crm.api;
+
+import com.thinkgem.jeesite.common.config.Global;
+import com.thinkgem.jeesite.common.persistence.Page;
+import com.thinkgem.jeesite.common.utils.StringUtils;
+import com.xiang.modules.common.api.BaseApi;
+import com.xiang.modules.crm.entity.CrmCustomer;
+import com.xiang.modules.crm.service.CrmCustomerService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 客户管理Controller
+ * @author Xiang
+ * @version 2017-02-28
+ */
+@Controller
+@RequestMapping(value = "${apiPath}/crm/crmCustomer")
+public class CrmCustomerApi extends BaseApi {
+
+	@Autowired
+	private CrmCustomerService crmCustomerService;
+	
+	@ModelAttribute
+	public CrmCustomer get(@RequestParam(required=false) String id) {
+		CrmCustomer entity = null;
+		if (StringUtils.isNotBlank(id)){
+			entity = crmCustomerService.get(id);
+		}
+		if (entity == null){
+			entity = new CrmCustomer();
+		}
+		return entity;
+	}
+	
+	@RequiresPermissions("crm:crmCustomer:view")
+	@RequestMapping(value = {"list", ""})
+	public String list(CrmCustomer crmCustomer, HttpServletRequest request, HttpServletResponse response, Model model) {
+		Page<CrmCustomer> page = crmCustomerService.findPage(new Page<CrmCustomer>(request, response), crmCustomer); 
+		model.addAttribute("page", page);
+		return "modules/crm/crmCustomerList";
+	}
+
+	@RequiresPermissions("crm:crmCustomer:view")
+	@RequestMapping(value = "form")
+	public String form(CrmCustomer crmCustomer, Model model) {
+		model.addAttribute("crmCustomer", crmCustomer);
+		return "modules/crm/crmCustomerForm";
+	}
+
+	@RequiresPermissions("crm:crmCustomer:edit")
+	@RequestMapping(value = "save")
+	public String save(CrmCustomer crmCustomer, Model model, RedirectAttributes redirectAttributes) {
+		if (!beanValidator(model, crmCustomer)){
+			return form(crmCustomer, model);
+		}
+		crmCustomerService.save(crmCustomer);
+		addMessage(redirectAttributes, "保存客户成功");
+		return "redirect:"+Global.getAdminPath()+"/crm/crmCustomer/?repage";
+	}
+	
+	@RequiresPermissions("crm:crmCustomer:edit")
+	@RequestMapping(value = "delete")
+	public String delete(CrmCustomer crmCustomer, RedirectAttributes redirectAttributes) {
+		crmCustomerService.delete(crmCustomer);
+		addMessage(redirectAttributes, "删除客户成功");
+		return "redirect:"+Global.getAdminPath()+"/crm/crmCustomer/?repage";
+	}
+
+}
